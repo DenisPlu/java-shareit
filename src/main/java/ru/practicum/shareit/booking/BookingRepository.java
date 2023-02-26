42e342f63ee94ce778b5a7dfcd68b03f79fa3893
Denis <den-dom@yandex.ru>
2023-02-26 12:34:15 +0300

    fix: correct requests from reviewer in PR

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -7,16 +7,16 @@ import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-    @Query(value = "SELECT * FROM BOOKINGS WHERE (BOOKER_ID = ?) ORDER BY ID DESC LIMIT ? OFFSET ?", nativeQuery = true)
+    @Query(value = "SELECT * FROM bookings WHERE (booker_id = ?) ORDER BY id DESC LIMIT ? OFFSET ?", nativeQuery = true)
     List<Booking> findByBookerIdWithPagination(Long ownerId, Integer from, Integer size);
 
-    @Query(value = "SELECT * FROM BOOKINGS WHERE (BOOKER_ID = ?) ORDER BY ID DESC OFFSET ?", nativeQuery = true)
+    @Query(value = "SELECT * FROM bookings WHERE (booker_id = ?) ORDER BY id DESC OFFSET ?", nativeQuery = true)
     List<Booking> findByBookerIdWithoutPagination(Long ownerId, Integer from);
 
     List<Booking>  findByItemId(Long itemId);
-    @Query(value = "SELECT * FROM BOOKINGS WHERE (ITEM_ID = ?) ORDER BY ID DESC LIMIT ? OFFSET ?", nativeQuery = true)
+    @Query(value = "SELECT * FROM bookings WHERE (item_id = ?) ORDER BY id DESC LIMIT ? OFFSET ?", nativeQuery = true)
     List<Booking>  findByItemIdWithPagination(Long itemId, Integer from, Integer size);
 
-    @Query(value = "SELECT * FROM BOOKINGS WHERE (ITEM_ID = ?) ORDER BY ID DESC OFFSET ?", nativeQuery = true)
+    @Query(value = "SELECT * FROM bookings WHERE (item_id = ?) ORDER BY id DESC OFFSET ?", nativeQuery = true)
     List<Booking>  findByItemIdWithoutPagination(Long itemId, Integer from);
 }
--- src/main/java/ru/practicum/shareit/request/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
     List<ItemRequest> findByRequesterId(Long requesterId);
 
-    @Query(value = "SELECT * FROM ITEM_REQUESTS WHERE (REQUESTER_ID <> ?) LIMIT ? OFFSET ?", nativeQuery = true)
+    @Query(value = "SELECT * FROM item_requests WHERE (requester_id <> ?) LIMIT ? OFFSET ?", nativeQuery = true)
     List<ItemRequest> searchFromWithLimit(Long ownerId, Integer size, Integer from);
 
-    @Query(value = "SELECT * FROM ITEM_REQUESTS WHERE (REQUESTER_ID <> ?) OFFSET ?", nativeQuery = true)
+    @Query(value = "SELECT * FROM item_requests WHERE (requester_id <> ?) OFFSET ?", nativeQuery = true)
     List<ItemRequest> searchFromWithoutLimit(Long ownerId, Integer from);
 }
